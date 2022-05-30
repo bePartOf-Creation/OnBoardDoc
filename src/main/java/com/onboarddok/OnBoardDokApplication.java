ca2f13d7b00ddab3e3121b95ab39f362c85b83d1
@bePartOfCreation <olasunkanmi.olayinka@ssafllc.com>
2022-05-30 13:00:12 +0100

    first commit

---
+++ src/main/java/com/onboarddok/OnBoardDokApplication.java
@@ -0,0 +1,13 @@
+package com.onboarddok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnBoardDokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnBoardDokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onboarddok/OnBoardDokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboarddok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnBoardDokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
